b67131e1b32cf0e21e5ed577c340b97370393001
Shaurnav.Ghosh <55925831+Shaurnav@users.noreply.github.com>
2023-08-18 00:42:30 -0700

    fleshed out input buttons with state

--- web/components/board/guess-entry/index.tsx
+++ web/components/board/guess-entry/index.tsx
@@ -1,7 +1,49 @@
+import { ChangeEvent, ReactHTMLElement, useState } from 'react';
+import styles from './styles.module.scss'
+import React from 'react';
 
-export default function GuessEntry() {
+export type GuessEntryProps = {
+  revealed: string,
+  initialDisabled: boolean,
+}
+
+//note to self, if we want to fully lock in the user, we are 
+//most likely going to have to use inputRef in order to ensure
+//that the user can't select anything else...
+export default function GuessEntry({revealed, initialDisabled}: GuessEntryProps) {
+  const [input, setInput] = useState(revealed);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  // const inputRef = React.useRef<HTMLInputElement>();
+
+  // const handleBlur = () => {
+  //   inputRef?.current?.focus();
+  // };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    
+  }
+
+  const handleSelect = () => {
+    console.log('hello');
+  }
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length >= revealed.length) {
+      setInput(event.target.value);
+    }     
+  }
 
   return (
-    <input/>
-  )
+   <input 
+    className={!disabled ? styles.input : styles.startAndEnd}
+    disabled={disabled}
+    type='text' 
+    value={input} 
+    onChange={handleChange}  
+    onClick={handleSelect}
+    onBlur={() => setInput(revealed)}
+    // ref={inputRef as React.MutableRefObject<HTMLInputElement>}
+   />
+  );
 }
--- web/components/board/index.tsx
+++ web/components/board/index.tsx
@@ -1,21 +1,66 @@
+import { useEffect, useState } from "react"
 import GuessEntry from "./guess-entry"
 import styles from './styles.module.scss'
 
-const state = {
+const initialState = {
   solution: ["hello", "world", "coin", "base"],
-  layout: ["hello", "", "", "base"],
+  layout: [
+    {
+      word: "hello",
+      revealed: "hello",
+    },
+    {
+      word: "world",
+      revealed: "w",
+    },
+    {
+      word: "coin",
+      revealed: "",
+    },
+    {
+      word: "base",
+      revealed: "base",
+    },
+  ],
   clues: 0,
+  choosing: true,
 }
 
-export default function Board() {
+interface IWord {
+  word: string,
+  revealed: string,
+}
+interface IState {
+  solution: string[],
+  layout: IWord[],
+  clues: number,
+  choosing: boolean,  
+}
 
+//if we are not choosing, we've selected a given piece, then
+//the background color for that specific gamepiece is solid...
+export default function Board() {
+  const [state, setState] = useState<IState>(initialState);
+
+  useEffect(() => {
+
+  }, [state?.choosing])
 
   return (
     <div className={styles.gameContainer}>
       <h1>game</h1>
-      <GuessEntry/>
-      <GuessEntry/>
-      <GuessEntry/>
+      {
+        state?.layout.map(({word, revealed}, index) => {
+          let disabled = false;
+          if (revealed.length === word.length) {
+            disabled = true;
+          }
+
+
+          return <GuessEntry key={index} revealed={revealed} initialDisabled={disabled}/>
+        })
+      }
+      
     </div>
   )
 
