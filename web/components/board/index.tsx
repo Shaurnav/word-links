f2979e348b615c44ab4610405b8cc8cab5ffefc4
Shreyas Sharma <129635111+shreyassharma1@users.noreply.github.com>
2024-06-19 23:40:34 -0700

    Update index.tsx

--- web/components/board/index.tsx
+++ web/components/board/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react"
-import GuessEntry from "./guess-entry"
-import styles from './styles.module.scss'
 import { IState } from "@/interfaces";
+import { useEffect, useState } from "react";
+import GuessEntry from "./guess-entry";
+import styles from './styles.module.scss';
 
 // Function to generate the initial state of the game board based on the date
 const generateInitialState = () => {
@@ -34,6 +34,7 @@ const generateInitialState = () => {
       clues: 0,
       choosing: true,
     }
+    
 
   ];
 
