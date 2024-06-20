eafe476c83ced593b3d4baf8ea7f2a7e55b3357d
Shreyas Sharma <129635111+shreyassharma1@users.noreply.github.com>
2024-06-20 00:05:21 -0700

    Update index.tsx

--- web/components/board/index.tsx
+++ web/components/board/index.tsx
@@ -11,7 +11,7 @@ const generateInitialState = () => {
   const puzzles = [
     {
       layout: [
-        { word: "brain", revealed: "brain1" },
+        { word: "brain", revealed: "brain" },
         { word: "stem", revealed: "s" },
         { word: "cell", revealed: "" },
         { word: "phone", revealed: "" },
