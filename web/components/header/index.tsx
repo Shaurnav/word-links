2d73ac88b1c71d0cb16f3a02852dbad2033b0eea
Shaurnav Ghosh <shaurnav@amazon.com>
2024-06-26 09:49:17 -0700

    initial commit on this branch to create dummy app

--- web/components/header/index.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Header() {
-  return (
-    <div>
-      <h1>WordLinks</h1>
-    </div>
-  );
-}
--- web/interfaces/index.ts
+++
@@ -1,9 +0,0 @@
-export interface IWord {
-  word: string,
-  revealed: string,
-}
-export interface IState {
-  layout: IWord[],
-  clues: number,
-  choosing: boolean,  
-}
